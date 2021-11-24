04755a183f736bcc4ad46f590c105c7fd5aa3ff5
satvikkathpal <satvikkathpal0109@gmail.com>
2021-11-24 14:47:41 -0500

    Initial Commit

---
+++ Satvik_Friends/Satvik_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Satvik_FriendsApp.swift
+//  Satvik_Friends
+//
+//  Created by Satvik Kathpal on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Satvik_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
